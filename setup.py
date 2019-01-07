c71c81e716981b3673e63ef18b8d7f6c9c29ef72
huidongchen <gjlmile@gmail.com>
2019-01-06 20:15:23 -0500

    fix package name

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ if sys.version_info.major != 3:
     raise RuntimeError('STREAM-atac requires Python 3')
 
 
-setup(name='stream-atac',
+setup(name='stream_atac',
       version="0.1.0",
       description='Single-cell Trajectories Reconstruction, Exploration And Mapping of single-cell data. Preprocessing for single cell atac-seq data',
       url='https://github.com/pinellolab/stream-atac',
       author='Huidong Chen',
       author_email='huidong.chen AT mgh DOT harvard DOT edu',
       license='Affero',
-      packages=['stream-atac'],
-      package_dir={'stream-atac': 'stream-atac'},
+      packages=['stream_atac'],
+      package_dir={'stream_atac': 'stream_atac'},
       install_requires=[''],)
 
