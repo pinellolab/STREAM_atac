7893120f2ca045e4b220c4e5d7f8b5a87a76a9ad
huidongchen <gjlmile@gmail.com>
2019-02-10 12:41:28 -0500

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info.major != 3:
 
 
 setup(name='stream_atac',
-      version="0.3.0",
+      version="0.3.1",
       description='STREAM preprocessing for single cell atac-seq data',
       url='https://github.com/pinellolab/stream-atac',
       author='Huidong Chen',
