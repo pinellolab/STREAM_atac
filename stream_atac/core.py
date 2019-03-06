f64e201dcb77e254e186f1cf54c954da335f4fa9
huidongchen <hd7chen@gmail.com>
2019-03-06 18:03:59 -0500

    add 10x support

--- stream_atac/core.py
+++ stream_atac/core.py
@@ -12,17 +12,27 @@ from rpy2.robjects import pandas2ri
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-def read(file_name,file_path='',file_format='tsv',delimiter='\t',experiment='rna-seq', workdir=None,**kwargs):
-    """Read gene expression matrix into anndata object.
+def preprocess_atac(file_count=None,file_region=None,file_sample=None,file_format='tsv',k=7,n_jobs = multiprocessing.cpu_count(),file_path='',delimiter='\t',workdir=None,**kwargs):
+    """Preprocess single cell atac-seq data and genearate a scaled z-score matrix.
     
     Parameters
     ----------
-    file_name: `str`
-        File name. For atac-seq data, it's the z-score matrix file name.
+    file_count: `str`
+        Count file name. A compressed matrix in sparse format (column-oriented).
+    file_region: `str`
+        Region file name. It has one column. Each row is a cell name (No header should be included). 
+    file_sample: `str`
+        Sample file name. It has three columns, i.e.,chromosome names, the start position of the region, the end position of the region (No header should be included).
+    file_format: `str`, optional (default: 'tsv')
+        File format of file_count. Currently supported file formats: 'tsv','txt','csv','mtx'.
+    k: `int`, optional (default: 7)
+        k mer.  
+    n_jobs: `int`, optional (default: all available cpus)
+        The number of parallel jobs to run
+    delimiter: `str`, optional (default: '\t')
+        Delimiter to use.
     file_path: `str`, optional (default: '')
         File path. By default it's empty
-    file_format: `str`, optional (default: 'tsv')
-        File format. currently supported file formats: 'tsv','txt','tab','data','csv','mtx','h5ad','pklz','pkl'
     delimiter: `str`, optional (default: '\t')
         Delimiter to use.
     experiment: `str`, optional (default: 'rna-seq')
@@ -33,53 +43,92 @@ def read(file_name,file_path='',file_format='tsv',delimiter='\t',experiment='rna
    
     Returns
     -------
-    AnnData object
-    """       
-    _fp = lambda f:  os.path.join(file_path,f)
-    if(file_format == 'pkl'):
-        if file_name.split('.')[-1]=='gz':
-            f = gzip.open(_fp(file_name), 'rb')
-            adata = pickle.load(f)
-            f.close() 
-        else:
-            f = open(_fp(file_name), 'rb')
-            adata = pickle.load(f)
-            f.close()    
-    elif(file_format == 'pklz'):
-        f = gzip.open(_fp(file_name), 'rb')
-        adata = pickle.load(f)
-        f.close()
+    AnnData object with the following fields:
+    
+    X : `numpy.ndarray` (`adata.X`)
+        A #observations × #k-mers scaled z-score matrix.
+    z_score: `numpy.ndarray` (`adata.layers['z_score']`)
+        A #observations × #k-mers z-score matrix.
+    atac-seq: `dict` (`adata.uns['atac-seq']`)   
+        A dictionary containing the following keys:
+        'count': (`adata.uns['atac-seq']['count']`), dataframe in sparse format, 
+                the first column specifies the rows indices (the regions) for non-zero entry. 
+                the second column specifies the columns indices (the sample) for non-zero entry. 
+                the last column contains the number of reads in a given region for a particular cell.
+        'region': (`adata.uns['atac-seq']['region']`), dataframe
+                the first column specifies chromosome names.
+                the second column specifies the start position of the region.
+                the third column specifies the end position of the region.
+        'sample': (`adata.uns['atac-seq']['sample']`), dataframe, the name of samples
+    """
+    
+    if(sum(list(map(lambda x: x is not None,[file_count,file_region,file_sample])))<3):
+        print('count file, region file, and sample file should all be provided.')
+        return
     else:
-        if(experiment not in ['rna-seq','atac-seq']):
-            print('The experiment '+experiment +' is not supported')
-            return         
-        if(file_format in ['tsv','txt','tab','data']):
-            adata = ad.read_text(_fp(file_name),delimiter=delimiter,**kwargs).T
-            adata.raw = adata        
-        elif(file_format == 'csv'):
-            adata = ad.read_csv(_fp(file_name),delimiter=delimiter,**kwargs).T
-            adata.raw = adata
+        print('Importing packages...')
+        chromVAR = importr('chromVAR')
+        GenomicRanges = importr('GenomicRanges')
+        SummarizedExperiment = importr('SummarizedExperiment')
+        BSgenome_Hsapiens_UCSC_hg19 = importr('BSgenome.Hsapiens.UCSC.hg19')
+        r_Matrix = importr('Matrix')
+        BiocParallel = importr('BiocParallel')
+        BiocParallel.register(BiocParallel.MulticoreParam(n_jobs))
+        pandas2ri.activate()
+        
+        if(workdir==None):
+            workdir = os.path.join(os.getcwd(), 'stream_result')
+            print("Using default working directory.")
+        if(not os.path.exists(workdir)):
+            os.makedirs(workdir)
+        print('Saving results in: %s' % workdir) 
+        
+        _fp = lambda f:  os.path.join(file_path,f)
+        print('Read in files ...')
+        if(file_format in ['tsv','txt','csv']):
+            df_counts = pd.read_csv(file_count,sep=delimiter,header=None,names=['i','j','x'],compression= 'gzip' if file_count.split('.')[-1]=='gz' else None)
         elif(file_format == 'mtx'):
-            adata = ad.read_mtx(_fp(file_name),**kwargs).T 
-            adata.X = np.array(adata.X.todense())
-            print(_fp(os.path.join(os.path.dirname(file_name),'genes.tsv')))
-            genes = pd.read_csv(_fp(os.path.join(os.path.dirname(file_name),'genes.tsv')), header=None, sep='\t')
-            adata.var_names = genes[1]
-            adata.var['gene_ids'] = genes[0].values
-            print(_fp(os.path.join(os.path.dirname(file_name),'barcodes.tsv')))
-            adata.obs_names = pd.read_csv(_fp(os.path.join(os.path.dirname(file_name),'barcodes.tsv')), header=None)[0]
-            adata.raw = adata
-        elif(file_format == 'h5ad'):
-            adata = ad.read_h5ad(_fp(file_name),**kwargs)
+            adata_atac = ad.read_mtx(_fp(file_count),**kwargs).T
+            mat = adata_atac.X.tocoo()
+            df_counts = pd.DataFrame(np.column_stack((mat.col+1,mat.row+1,mat.data)),columns=['i','j','x'],dtype=int)
         else:
             print('file format ' + file_format + ' is not supported')
-            return
-        adata.uns['experiment'] = experiment        
-    if(workdir==None):
-        workdir = os.path.join(os.getcwd(), 'stream_result')
-        print("Using default working directory.")
-    if(not os.path.exists(workdir)):
-        os.makedirs(workdir)
-    adata.uns['workdir'] = workdir
-    print('Saving results in: %s' % workdir)
-    return adata
+            return        
+        df_regions = pd.read_csv(file_region,sep=delimiter,header=None,compression= 'gzip' if file_region.split('.')[-1]=='gz' else None)
+        df_regions = df_regions.iloc[:,:3]
+        df_regions.columns = ['seqnames','start','end']
+        df_samples = pd.read_csv(file_sample,sep=delimiter,header=None,names=['cell_id'],compression= 'gzip' if file_sample.split('.')[-1]=='gz' else None)                           
+
+        r_regions_dataframe = pandas2ri.py2ri(df_regions)
+        regions = GenomicRanges.makeGRangesFromDataFrame(r_regions_dataframe)
+        counts = r_Matrix.sparseMatrix(i = df_counts['i'], j = df_counts['j'], x=df_counts['x'])
+        samples = pandas2ri.py2ri(df_samples)
+        samples.rownames = df_samples['cell_id']
+
+        SE = SummarizedExperiment.SummarizedExperiment(rowRanges = regions,colData = samples,assays = robjects.ListVector({'counts':counts}))
+        SE = chromVAR.addGCBias(SE, genome = BSgenome_Hsapiens_UCSC_hg19.BSgenome_Hsapiens_UCSC_hg19)
+
+        # compute kmer deviations
+        print('Computing k-mer deviations...')
+        KmerMatch = chromVAR.matchKmers(k, SE, BSgenome_Hsapiens_UCSC_hg19.BSgenome_Hsapiens_UCSC_hg19)
+        BiocParallel.register(BiocParallel.SerialParam())
+        Kmerdev = chromVAR.computeDeviations(SE, KmerMatch)
+        KmerdevTable = SummarizedExperiment.assays(Kmerdev)
+        cn = pandas2ri.ri2py((Kmerdev.do_slot('colData')).do_slot('listData').rx2('cell_id'))
+        rn = pandas2ri.ri2py(Kmerdev.do_slot('NAMES'))
+        scores = pandas2ri.ri2py(KmerdevTable.do_slot('listData').rx2('deviations'))    
+
+        df_zscores = pd.DataFrame(scores,index=rn,columns=cn)
+        df_zscores_scaled = preprocessing.scale(df_zscores,axis=1)
+        df_zscores_scaled = pd.DataFrame(df_zscores_scaled,index=df_zscores.index,columns=df_zscores.columns)
+        df_zscores_scaled.to_csv(os.path.join(workdir,'zscore.tsv.gz'),sep = '\t',compression='gzip')
+        adata = ad.AnnData(X=df_zscores_scaled.values.T, obs={'obs_names':df_zscores_scaled.columns},var={'var_names':df_zscores_scaled.index})
+        adata.raw = adata
+        adata.uns['workdir'] = workdir
+        adata.uns['experiment'] = 'atac-seq'
+        adata.layers["z_score"] = df_zscores.values.T
+        adata.uns['atac-seq'] = dict()
+        adata.uns['atac-seq']['count'] = df_counts
+        adata.uns['atac-seq']['region'] = df_regions
+        adata.uns['atac-seq']['sample'] = df_samples
+        return adata
